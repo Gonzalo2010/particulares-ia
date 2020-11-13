af639ccbfcf6640fe6685400f37ae1ea3555a819
Evaristo Dantur <evaristo@dantur.com>
2020-11-13 17:00:45 -0300

    nodemon agregado

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/productos", (req, res, next) =>{
+  res.send("hola")
+})
+
 module.exports = router;
